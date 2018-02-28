a1c96b8c324078b82e92fdbcb53e051d0862b382
Yury.Kerbiczkov <yury.kerbiczkov@e-lab.icl.kazan.ru>
2018-02-28 19:21:44 +0300

    menu improvements

--- UWP.SimpleRtspPlayer/SimpleRtspPlayer/MainPage.xaml.cs
+++ UWP.SimpleRtspPlayer/SimpleRtspPlayer/MainPage.xaml.cs
@@ -12,47 +12,40 @@ namespace SimpleRtspPlayer
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
     using Windows.UI.Xaml.Controls.Primitives;
-    using Windows.UI.Xaml.Markup;
 
     public sealed partial class MainPage
     {
         private readonly ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
-
-        private NavigationViewItem prevNavItem;
-        private NavigationViewItem addRtspNavigationViewItem;
-        private NavigationViewItem removeNavigationItem;
 
         private ApplicationDataCompositeValue listOfCameras;
 
         public MainPage()
         {
             InitializeComponent();
-            AddAddMenuItem();
-            AddRemoveMenuItem();
             IconPicker.ItemsSource = Enum.GetValues(typeof(Symbol)).Cast<Symbol>();
             IconPicker.SelectedItem = Symbol.Home;
             ReadCameras();
         }
 
-        public ObservableCollection<NavigationViewItemBase> MenuItems { get; set; } =
-            new ObservableCollection<NavigationViewItemBase>();
+        public NavigationViewItem SelectedNavItem { get; set; }
+
+        public ObservableCollection<NavigationViewItem> MenuItems { get; set; } =
+            new ObservableCollection<NavigationViewItem>();
 
         private void AddNewStreamOnTapped(object sender, TappedRoutedEventArgs e)
         {
-            removeNavigationItem.IsEnabled = false;
             FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
         }
 
         private void RemoveStreamOnTapped(object sender, TappedRoutedEventArgs e)
         {
-            removeNavigationItem.IsEnabled = false;
             FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
         }
 
         private async void ButtonOkOnTapped(object sender, TappedRoutedEventArgs e)
         {
-            if (MenuItems.Where(cam => cam is NavigationViewItem)
-                .Select(cam => (cam as NavigationViewItem).Content.ToString()).Contains(CameraName.Text))
+            if (MenuItems.Where(cam => cam.Content != null).Select(cam => cam.Content.ToString())
+                .Contains(CameraName.Text))
             {
                 var loader = new ResourceLoader();
                 MessageDialog dialog = new MessageDialog(loader.GetString("CameraSameNameError"));
@@ -62,7 +55,8 @@ namespace SimpleRtspPlayer
 
             var navItem = AddCameraNavItem();
 
-            listOfCameras[navItem.Content.ToString()] = $"{((SymbolIcon)(navItem.Icon)).Symbol},{navItem.Tag}";
+            if (navItem.Content != null)
+                listOfCameras[navItem.Content.ToString()] = $"{((SymbolIcon)(navItem.Icon)).Symbol},{navItem.Tag}";
             localSettings.Values["listOfCameras"] = listOfCameras;
 
             NewRtspStreamFlyout.Hide();
@@ -70,17 +64,14 @@ namespace SimpleRtspPlayer
 
         private NavigationViewItem AddCameraNavItem(string name = null, Symbol? icon = null, string cameraUri = null)
         {
-            if (MenuItems.Count == 2) AddNavigationViewItemHeader();
-
             var navItem = new NavigationViewItem
                               {
                                   Content = name ?? CameraName.Text,
                                   Icon = new SymbolIcon(
                                       icon ?? (Symbol)(IconPicker.SelectedItem ?? Symbol.Home)),
-                                  Tag = cameraUri ?? CameraUri.Text
+                                  Tag = cameraUri ?? CameraUri.Text,
+                                  CanDrag = true
                               };
-            navItem.Tapped += CameraTapped;
-            navItem.CanDrag = true;
 
             MenuItems.Add(navItem);
             return navItem;
@@ -91,63 +82,17 @@ namespace SimpleRtspPlayer
             NewRtspStreamFlyout.Hide();
         }
 
-        private void AddAddMenuItem()
+        private void RemoveButtonCancelOnTapped(object sender, TappedRoutedEventArgs e)
         {
-            addRtspNavigationViewItem = (NavigationViewItem)XamlReader.Load(
-                "<NavigationViewItem "
-                + "xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" "
-                + "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" "
-                + "x:Uid=\"AddCameraNavItem\" "
-                + "Icon=\"Add\" />");
-
-            FlyoutBase.SetAttachedFlyout(addRtspNavigationViewItem, (Flyout)Resources["NewRtspStreamFlyout"]);
-            addRtspNavigationViewItem.Tapped += AddNewStreamOnTapped;
-            MenuItems.Add(addRtspNavigationViewItem);
-        }
-
-        private void AddNavigationViewItemHeader()
-        {
-            var navItem = (NavigationViewItemHeader)XamlReader.Load(
-                "<NavigationViewItemHeader "
-                + "xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" "
-                + "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" "
-                + "x:Uid=\"CamerasNavViewItemHeader\" "
-                + "Margin=\"33,0,0,0\" />");
-
-            MenuItems.Add(navItem);
-        }
-
-        private void CameraTapped(object sender, TappedRoutedEventArgs e)
-        {
-            removeNavigationItem.IsEnabled = true;
-        }
-
-        private void AddRemoveMenuItem()
-        {
-            if (removeNavigationItem == null)
-            {
-                removeNavigationItem = (NavigationViewItem)XamlReader.Load(
-                    "<NavigationViewItem " + "xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" "
-                    + "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" "
-                    + "x:Uid=\"RemoveCameraNavItem\" "
-                    + "Icon=\"Remove\" />");
-                removeNavigationItem.IsEnabled = false;
-
-                FlyoutBase.SetAttachedFlyout(removeNavigationItem, (Flyout)Resources["RemoveRtspStreamFlyout"]);
-                removeNavigationItem.Tapped += RemoveStreamOnTapped;
-            }
-
-            MenuItems.Add(removeNavigationItem);
+            RemoveRtspStreamFlyout.Hide();
         }
 
         private void RemoveOkTapped(object sender, TappedRoutedEventArgs e)
         {
-            listOfCameras.Remove(prevNavItem.Content?.ToString());
+            listOfCameras.Remove(SelectedNavItem.Content?.ToString());
             localSettings.Values["listOfCameras"] = listOfCameras;
 
-            MenuItems.Remove(prevNavItem);
-
-            if (MenuItems.Count == 3) MenuItems.RemoveAt(2);
+            MenuItems.Remove(SelectedNavItem);
 
             RemoveRtspStreamFlyout.Hide();
         }
@@ -155,14 +100,6 @@ namespace SimpleRtspPlayer
         private void RemoveCancelTapped(object sender, TappedRoutedEventArgs e)
         {
             RemoveRtspStreamFlyout.Hide();
-        }
-
-        private void NavigationView_OnSelectionChanged(
-            NavigationView sender,
-            NavigationViewSelectionChangedEventArgs args)
-        {
-            if (!args.SelectedItem.Equals(addRtspNavigationViewItem) && !args.SelectedItem.Equals(removeNavigationItem))
-                prevNavItem = args.SelectedItem as NavigationViewItem;
         }
 
         private void FrameworkElement_OnLoaded(object sender, RoutedEventArgs e)
@@ -192,6 +129,11 @@ namespace SimpleRtspPlayer
                     cameraParams.Length - indexOfSemiColon - 1);
                 AddCameraNavItem(cameraName, icon, cameraUri);
             }
+        }
+
+        private void NavigationViewOnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            SelectedNavItem = args.SelectedItem as NavigationViewItem;
         }
     }
 }
---
+++ UWP.SimpleRtspPlayer/SimpleRtspPlayer/ReferenceTypeToBoolConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleRtspPlayer
+{
+    using System;
+
+    using Windows.UI.Xaml.Data;
+
+    public class ReferenceTypeToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => value != null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+            throw new NotSupportedException();
+    }
+}
